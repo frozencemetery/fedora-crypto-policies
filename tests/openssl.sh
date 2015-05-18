ffe885eccfd3fab541e3707a6adebd8544f6dd9e
Nikos Mavrogiannopoulos <nmav@redhat.com>
2015-05-18 11:09:09 +0200

    openssl: updated tests to allow empty ciphers

--- tests/openssl.sh
+++ tests/openssl.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+TMPFILE=out-openssl.tmp
 for i in profiles/*.settings;do
 	. $i
-	openssl ciphers $CONFIG_OPENSSL >/dev/null
-	if test $? != 0;then
+	openssl ciphers $CONFIG_OPENSSL >$TMPFILE 2>&1
+	if test $? != 0 && test $i != "profiles/EMPTY.settings";then
+		echo "Error in $i"
+		cat $TMPFILE
 		exit 1
 	fi
 done
+
+rm -f $TMPFILE
 
 exit 0
