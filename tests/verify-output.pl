43503edf745e4c079f80e2e905f378f7ad8b8f84
Nikos Mavrogiannopoulos <nmav@redhat.com>
2017-05-15 10:00:53 +0200

    verify-output: print to stderr
    Signed-off-by: Nikos Mavrogiannopoulos <nmav@redhat.com>

--- tests/verify-output.pl
+++ tests/verify-output.pl
@@ -27,8 +27,8 @@ foreach $mod (@modules) {
     	    $/ = "\n";
 
 	    if ($tmp ne $contents) {
-	        print "\nError in the contents of $profile-$mod.txt\n";
-	        print "If the changes in the output policies are expected run make 'reset-outputs' and verify the result\n";
+	        print STDERR "\nError in the contents of $profile-$mod.txt\n";
+	        print STDERR "If the changes in the output policies are expected run make 'reset-outputs' and verify the result\n";
 	        exit 1;
 	    }
         } else {
