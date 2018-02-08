30d59369097349714495dde95cf6f281dbcf56a7
Nikos Mavrogiannopoulos <nmav@redhat.com>
2018-02-08 10:41:30 +0100

    LEGACY policy: switched to Fedora strong crypto settings requirements
    LEGACY
     MACs: All HMAC with SHA1 or better + all modern MACs (poly1305 etc)
     Curves: all prime >= 255 bits (including bernstein curves)
     Signature algorithms: SHA-1 hash or better (not RIPEMD)
     Ciphers: all available > 112-bit key, >= 128-bit block (no rc4, but with 3DES)
     key exchange: ECDHE, RSA, DHE
     DH params size: >=1024
     RSA params size: >=1024
     TLS protocols: TLS >= 1.0
    
    https://fedoraproject.org/wiki/Changes/StrongCryptoSettings
    
    Signed-off-by: Nikos Mavrogiannopoulos <nmav@redhat.com>

--- back-ends/profiles/LEGACY.pl
+++ back-ends/profiles/LEGACY.pl
@@ -1,21 +1,22 @@
 # A level that will ensure maximum compatibility with legacy systems.
 # It should provide at least 64-bit security and include RC4 and MD5 (for HMAC).
 
-# MACs: MD5, SHA1+
-# Groups: All supported
-# Signature algorithms: must use SHA-1 hash or better
-# (Note: signature algorithms restrictions shouldn't apply to self-signatures)
-# Ciphers: AES-GCM, AES-CCM, AES-CTR, AES-CBC, CAMELLIA-GCM, CAMELLIA-CBC, 3DES-CBC, RC4
-# Key exchange: ECDHE, RSA, DHE
-# DH params size: 767+
-# RSA params size: 767+
-# Protocols: All supported (SSL3.0+)
+# MACs: All HMAC with SHA1 or better + all modern MACs (poly1305 etc)
+# Curves: all prime >= 255 bits (including bernstein curves)
+# Signature algorithms: SHA-1 hash or better (not RIPEMD)
+# TLS Ciphers: all available > 112-bit key, >= 128-bit block (no rc4, but with 3DES)
+# non-TLS Ciphers: all available > 112-bit key, >= 128-bit block (no rc4, but with 3DES)
+# key exchange: ECDHE, RSA, DHE
+# DH params size: >=1023
+# RSA params size: >=1023
+# TLS protocols: TLS >= 1.0
 
 sub update_lists {
 
-    @mac_list = @full_mac_list;
-    @group_list = @full_group_list;
-    @hash_list = @full_hash_list;
+    @mac_list = ('AEAD', 'UMAC-128', 'HMAC-SHA1', 'HMAC-SHA2-256', 'HMAC-SHA2-384', 'HMAC-SHA2-512');
+    @group_list = ('X25519', 'X448', 'SECP256R1', 'SECP384R1', 'SECP521R1',
+        'FFDHE-2048', 'FFDHE-3072', 'FFDHE-4096', 'FFDHE-6144', 'FFDHE-8192');
+    @hash_list = ('SHA2-256', 'SHA2-384', 'SHA2-512', 'SHA3-256', 'SHA3-384', 'SHA3-512', 'SHA1');
     @sign_list = ('RSA-SHA1', 'DSA-SHA1', 'ECDSA-SHA1',
         'RSA-SHA2-224', 'DSA-SHA2-224', 'ECDSA-SHA2-224',
         'RSA-SHA2-256', 'DSA-SHA2-256', 'ECDSA-SHA2-256',
@@ -27,20 +28,21 @@ sub update_lists {
         'EDDSA-ED25519', 'EDDSA-ED448',
         'RSA-PSS-SHA1', 'RSA-PSS-SHA2-256', 'RSA-PSS-SHA2-384', 'RSA-PSS-SHA2-512');
 
-    @cipher_list = ('AES-256-GCM', 'AES-256-CCM', 'CHACHA20-POLY1305', 'CAMELLIA-256-GCM',
+    @tls_cipher_list = ('AES-256-GCM', 'AES-256-CCM', 'CHACHA20-POLY1305', 'CAMELLIA-256-GCM',
         'AES-256-CTR', 'AES-256-CBC', 'CAMELLIA-256-CBC', 
         'AES-128-GCM', 'AES-128-CCM', 'CAMELLIA-128-GCM', 'AES-128-CTR', 'AES-128-CBC', 'CAMELLIA-128-CBC',
         '3DES-CBC', 'RC4-128');
-    @key_exchange_list = ('ECDHE', 'RSA', 'DHE', 'PSK', 'DHE-PSK', 'ECDHE-PSK');
-    @protocol_list = ('TLS1.2', 'TLS1.1', 'TLS1.0', 'SSL3.0', 'DTLS1.2', 'DTLS1.0');
+    @cipher_list = @tls_cipher_list;
+    @key_exchange_list = ('ECDHE', 'RSA', 'DHE-RSA', 'DHE-DSS', 'PSK', 'DHE-PSK', 'ECDHE-PSK');
+    @protocol_list = ('TLS1.2', 'TLS1.1', 'TLS1.0', 'DTLS1.2', 'DTLS1.0');
 
-    $min_tls_version = 'SSL3.0';
-    $min_dtls_version = 'SSL3.0';
+    $min_tls_version = 'TLS1.0';
+    $min_dtls_version = 'TLS1.0';
 
     # Parameter sizes
-    $min_dh_size = 767;
-    $min_dsa_size = 767;
-    $min_rsa_size = 767;
+    $min_dh_size = 1023;
+    $min_dsa_size = 1023;
+    $min_rsa_size = 1023;
 
     update_rev_lists();
 }
