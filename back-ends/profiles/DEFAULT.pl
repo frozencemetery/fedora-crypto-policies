f4ee1cc72e8da7237ddbfd2c9c20be4b7643089c
Nikos Mavrogiannopoulos <nmav@redhat.com>
2018-02-23 15:41:48 +0100

    switched order of algorithms to promote stronger hashes and signatures
    Signed-off-by: Nikos Mavrogiannopoulos <nmav@redhat.com>

--- back-ends/profiles/DEFAULT.pl
+++ back-ends/profiles/DEFAULT.pl
@@ -13,12 +13,11 @@
 
 sub update_lists {
 
-    @mac_list = ('AEAD', 'UMAC-128', 'HMAC-SHA1', 'HMAC-SHA2-256', 'HMAC-SHA2-384', 'HMAC-SHA2-512');
+    @mac_list = ('AEAD', 'HMAC-SHA2-256', 'HMAC-SHA1', 'UMAC-128', 'HMAC-SHA2-384', 'HMAC-SHA2-512');
     @group_list = ('X25519', 'X448', 'SECP256R1', 'SECP384R1', 'SECP521R1',
         'FFDHE-2048', 'FFDHE-3072', 'FFDHE-4096', 'FFDHE-6144', 'FFDHE-8192');
     @hash_list = ('SHA2-256', 'SHA2-384', 'SHA2-512', 'SHA3-256', 'SHA3-384', 'SHA3-512', 'SHA1');
-    @sign_list = ('RSA-SHA1', 'ECDSA-SHA1',
-        'RSA-SHA2-224', 'ECDSA-SHA2-224',
+    @sign_list = (
         'RSA-SHA2-256', 'ECDSA-SHA2-256',
         'RSA-SHA2-384', 'ECDSA-SHA2-384',
         'RSA-SHA2-512', 'ECDSA-SHA2-512',
@@ -26,12 +25,17 @@ sub update_lists {
         'RSA-SHA3-384', 'ECDSA-SHA3-384',
         'RSA-SHA3-512', 'ECDSA-SHA3-512',
         'EDDSA-ED25519', 'EDDSA-ED448',
-        'RSA-PSS-SHA1', 'RSA-PSS-SHA2-256', 'RSA-PSS-SHA2-384', 'RSA-PSS-SHA2-512');
+        'RSA-PSS-SHA2-256', 'RSA-PSS-SHA2-384', 'RSA-PSS-SHA2-512',
+        'RSA-SHA2-224', 'ECDSA-SHA2-224',
+        'RSA-PSS-SHA1', 'RSA-SHA1', 'ECDSA-SHA1');
 
     @tls_cipher_list = ('AES-256-GCM', 'AES-256-CCM', 'CHACHA20-POLY1305', 'CAMELLIA-256-GCM',
         'AES-256-CTR', 'AES-256-CBC', 'CAMELLIA-256-CBC', 
         'AES-128-GCM', 'AES-128-CCM', 'CAMELLIA-128-GCM', 'AES-128-CTR', 'AES-128-CBC', 'CAMELLIA-128-CBC');
     @cipher_list = @tls_cipher_list;
+
+# 'RSA' is intentionally before DHE ciphersuites, as the DHE ciphersuites have
+# interoperability issues in TLS.
     @key_exchange_list = ('ECDHE', 'RSA', 'DHE', 'DHE-RSA', 'PSK', 'DHE-PSK', 'ECDHE-PSK');
 #    @protocol_list = ('TLS1.2', 'DTLS1.2');
 #    $min_tls_version = 'TLS1.2';
